4a154e6ffe27fc65d0d9e669d909453fabf21096
Sawan <sawan.yadav2@gmail.com>
2021-04-03 16:53:59 -0700

    incomplete

---
+++ test5_incomplete.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+from typing import List
+
+class TreeNode:
+
+    def __init__(self, data: str = None, left:str = None, \
+                right: str = None):
+        pass
+
+
